c4b8a6d4b91522344c1728d3f5f34422cacea15b
Ricardo Huerta <ricardohc1988@gmail.com>
2024-02-06 14:16:18 -0600

    security and auth

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 router: DefaultRouter = DefaultRouter()
@@ -7,6 +8,8 @@ router.register(r'tables', views.BookingViewSet)
 
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('menu/', views.MenuItemsView.as_view()),
     path('booking/', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token)
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Booking, Menu
 from .serializers import BookingSerializer, MenuSerializer
 
+def index(request):
+    return render(request, 'index.html')
+
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
