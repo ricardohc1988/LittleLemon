d525cb021aae9af5e69a5af2c9b34290b769cf59
Ricardo Huerta <ricardohc1988@gmail.com>
2024-02-07 14:15:53 -0600

    modified urls.py file and test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,7 +6,3 @@ class MenuTest(TestCase):
     def test_get_item(self):
         item = Menu.objects.create(title="IceCream", price=80, inventory=100)
         self.assertEqual(item, "IceCream : 80")
-
-    # def test_create_item(self):
-    #     item = Menu.objects.create(title="IceCream", price=80, inventory=100)
-    #     self.assertEqual(str(item), "IceCream : 80")
