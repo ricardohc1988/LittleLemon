4d42ee594b471c83bfa71026426cf46fbdf29f39
Ricardo Huerta <ricardohc1988@gmail.com>
2024-02-07 15:16:53 -0600

    modified urls.py

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('menu/', views.MenuItemsView.as_view()),
-    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('menu/<int:pk>/', views.SingleMenuItemView.as_view()),
     path('api-token-auth/', obtain_auth_token)
 ]
